7003536d41dc5244e5b8fe8a87fc60fee7e11195
Yousef Soliman <yousef@arkway.com>
2013-04-08 00:53:08 +0100

    first typecheck commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Typecheck</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="typecheck.js"></script>
+</head>
+<body>
+	<form>
+		<input type="text" class="typecheck-character typecheck-min" data-typecheck-min="8">
+		<input type="text" class="typecheck-character typecheck-max" data-typecheck-max="4">
+		<input type="text" class="typecheck-character typecheck-eq" data-typecheck-eq="8">
+	</form>
+</body>
+</html>
